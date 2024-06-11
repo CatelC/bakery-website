b16cb84038c4a7ddd5c810a217dd8b11dcf89183
Clément Catel <117723517+CatelC@users.noreply.github.com>
2024-06-11 10:15:48 +0200

    Change README and possible favicon fix

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -4,11 +4,11 @@ export default function App({Component, pageProps} : AppProps){
     return (
         <>
         <head>
-            <link rel="icon" href="/favicon.ico" />
-            <link rel="icon" sizes="16x16" href="/favicon-16x16.png" />
-            <link rel="icon" sizes="32x32" href="/favicon-32x32.png" />
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-            <link rel="manifest" href="/manifest.webmanifest"/>
+            <link rel="icon" href="/favicon.ico?" />
+            <link rel="icon" sizes="16x16" href="./favicon-16x16.png" />
+            <link rel="icon" sizes="32x32" href="./favicon-32x32.png" />
+            <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+            <link rel="manifest" href="./manifest.webmanifest"/>
         </head>
         <Component {...pageProps} />
         </>
