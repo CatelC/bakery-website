2bdd4c349fb421e90e6399d17656d737fad42bdc
Clément Catel <117723517+CatelC@users.noreply.github.com>
2024-06-11 16:46:53 +0200

    Possible fix for online favicon

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,15 +1,18 @@
 import { AppProps } from "next/app"
+import Head from "next/head"
+import Link from "next/link"
+
 
 export default function App({Component, pageProps} : AppProps){
     return (
         <>
-        <head>
-            <link rel="icon" href="/favicon.ico?" />
+        <Head>
+            <link rel="icon" href="favicon.ico?" />
             <link rel="icon" sizes="16x16" href="./favicon-16x16.png" />
             <link rel="icon" sizes="32x32" href="./favicon-32x32.png" />
             <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
             <link rel="manifest" href="./manifest.webmanifest"/>
-        </head>
+        </Head>
         <Component {...pageProps} />
         </>
     )
