0d3b3f5570ce59b39fd64513f8b68af39c1dbf39
Clément Catel <117723517+CatelC@users.noreply.github.com>
2024-06-27 20:10:53 +0200

    Added Coming-Soon page

--- src/app/games/comingsoon/page.tsx
+++ src/app/games/comingsoon/page.tsx
@@ -1,7 +1,22 @@
 export default function comingsoon() {
     return (
-        <main>
+        <main className="game">
 
+            <div className="description mb-32 grid text-center ">
+                <a
+                    className="group border px-5 py-4 transition-colors"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2 className="mb-3 text-2xl font-semibold">
+                        What we are working on
+                    </h2>
+                    <p className="m-0 text-balance text-base">
+                        Right now, we are working on our first game : The Bakery (yes it is the same name as the studio). That being said, we have other games in mind that we would like to do. 
+                        You can follow our personnal socials and the studio on different platform for the most recent news about our games !
+                    </p>
+                </a>
+            </div>
         </main>
     );
 }
