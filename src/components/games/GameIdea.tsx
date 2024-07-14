66b1d6d1084bfe9da4f45334733a61204d485ae8
Clément Catel <117723517+CatelC@users.noreply.github.com>
2024-07-14 18:52:54 +0200

    GameIdea hover and bacground filter

--- src/components/games/GameIdea.tsx
+++ src/components/games/GameIdea.tsx
@@ -6,10 +6,19 @@ interface GameIdeaProps {
     backgroundImagePath?: string;
 }
 
+declare module "react" {
+    interface CSSProperties {
+        "--bg-image-game-idea"?: string;
+    }
+}
+
 const GameIdea: FC<GameIdeaProps> = ({ text, backgroundImagePath }) => {
     return (
-        <div className="gameIdea" style={{'--bg-image-game-idea': `url(${backgroundImagePath})`}}>
-            <p>{text}</p>
+        <div className="gameIdea" style={{ "--bg-image-game-idea": `url(${backgroundImagePath})` }}>
+            <div className="gameDiv">
+                <p>{text}</p>
+            </div>
+
         </div>
 
     );
